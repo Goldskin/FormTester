4c66941429f8f440b6641529c505275af83d9e6d
Charles Strube <charlesstrube@gmail.com>
2017-03-13 19:00:10 +0100

    Create FormTester.php

---
+++ FormTester.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * form tester
+ * @version 2.0
+ * @since 2.0
+ */
+class FormTester
+{
+
+
+    /**
+     * title constructor
+     * @param string $id code
+     * @param string $slug slug
+     * verifyNonce($_SERVER['REMOTE_ADDR'], 'contact-front')
+     */
+    public function constructNonce( $id, $slug )
+    {
+        ob_start();
+        wp_nonce_field( 'nonce-' . $id, $slug, false );
+        $out = ob_get_clean();
+        return $out;
+    }
+
+    /**
+     * title constructor
+     * @param string $id code
+     * @param string $slug slug
+     * verifyNonce($_SERVER['REMOTE_ADDR'], 'contact-front')
+     */
+    public function verifyNonce( $id, $slug )
+    {
+        return ( isset($_POST[$slug]) && wp_verify_nonce( $this->clean( $_POST[ $slug ] ), 'nonce-' . $id ) );
+    }
+
+    /**
+     * title constructor
+     * @param string $value submit text
+     */
+    public function clean( $value )
+    {
+        if ( isset($value) || isset($value) != null )
+            return trim( htmlspecialchars( $value ) );
+        return false;
+    }
+}
